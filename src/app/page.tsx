fc68caf09b54311a58b08cac29226b6b497158e9
최경훈 <idchoi2@gmail.com>
2024-10-08 11:17:08 +0900

    push to dev

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ export default async function HomePage() {
 
   return (
     <LayoutAdmin>
-      <Typography variant="h1">Hello, {user?.email}</Typography>
+      <Typography variant="h1">Hello! {user?.email}</Typography>
     </LayoutAdmin>
   )
 }
